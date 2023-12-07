18c88b122995356c38f963ef67262a8fcaa41ff9
Matija Osrečki <github@matijao.com>
2023-12-08 00:28:30 +0100

    feat: improve range

--- src/array/range.ts
+++ src/array/range.ts
@@ -1,4 +1,5 @@
-// TODO: make end exclusive
+// end is exclusive
+// third parameter is step, when descending
 export function range(len: number): number[]
 export function range(start: number, end: number, step?: number): number[]
 export function range(startOrLen: number, end?: number, step = 1): number[] {
@@ -7,17 +8,21 @@ export function range(startOrLen: number, end?: number, step = 1): number[] {
     startOrLen = 0
   }
 
-  if (step <= 0) {
+  const isAsc = startOrLen < end
+
+  if (isAsc && step < 0) {
     throw new Error('The step must be greater than 0.')
+  } else if (!isAsc && step > 0) {
+    throw new Error('The step must be less than 0.')
   }
 
-  step = startOrLen > end ? -step : step
-  const length = Math.floor(Math.abs((end - startOrLen) / step)) + 1
+  step = startOrLen > end ? -Math.abs(step) : step
+  const length = Math.ceil(Math.abs((end - startOrLen) / step))
 
   const result = Array.from<number>({ length })
 
   for (let i = 0; i < length; i++) {
-    result[i] = startOrLen + i * step
+    result[i] = startOrLen + (i * step)
   }
 
   return result
