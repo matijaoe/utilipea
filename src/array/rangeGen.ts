ff19b72bc5890804eb82526c7158d9d73269d7ca
Matija Osrečki <github@matijao.com>
2023-12-09 22:31:12 +0100

    docs: write jsdoc for range fns

--- src/array/rangeGen.ts
+++ src/array/rangeGen.ts
@@ -1,6 +1,40 @@
 import { isFunction } from '..'
 
-// TODO: handle reverse
+/**
+ * Yields values from start to end (exclusive), incrementing by step.
+ *
+ * Optionally transforms each value using a mapper function or a fixed value.
+ *
+ * With only startOrLength provided, generates values from 0 to length (exclusive).
+ * With start, end, and step, generates from start to end, adjusting by step.
+ *
+ * @example
+ * // Using in a for loop:
+ * for (const num of rangeGen(1, 5)) {
+ *   console.log(num)
+ * }
+ * // => 1 2 3 4
+ *
+ * // Using a mapper function:
+ * for (const square of rangeGen(1, 4, 1, (i) => i * i)) {
+ *   console.log(square)
+ * }
+ * // => 1 4 9
+ *
+ * // Using a step value:
+ * for (const num of rangeGen(0, 10, 3)) {
+ *   console.log(num)
+ * }
+ * // => 0 3 6 9
+ *
+ * @param startOrLength Start number or length of the sequence if end is not provided
+ * @param [end] End number of the sequence (exclusive), optional if only startOrLength is provided
+ * @param [step] Step between numbers
+ * @param [valueOrMapper] Fixed value or mapper function to transform each sequence element
+ * @throws If step is 0, or if start and end are equal without a mapper function.
+ * @returns Generator yielding values from 'start' to 'end' (exclusive), adjusted by 'step'
+ */
+
 export function* rangeGen<T = number>(
   startOrLength: number,
   end?: number,
