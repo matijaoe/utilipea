aa003aea921906e893b96287f3b943c9200f50c8
Matija Osrečki <github@matijao.com>
2023-12-10 15:55:42 +0100

    feat: new clamp

--- src/number/clamp.test.ts
+++ src/number/clamp.test.ts
@@ -2,28 +2,41 @@ import { describe, expect, it } from 'vitest'
 import { clamp } from './clamp'
 
 describe('clamp', () => {
-  it('clamp a number between a minimum and maximum value', () => {
-    expect(clamp(3, 1, 5)).toEqual(3)
-    expect(clamp(-3, -5, -1)).toEqual(-3)
+  it('number over min value', () => {
+    expect(clamp(3, { min: 1 })).toEqual(3)
+    expect(clamp(-3, { min: -5 })).toEqual(-3)
   })
 
-  it('clamp a number over maximum value', () => {
-    expect(clamp(7, 1, 5)).toEqual(5)
-    expect(clamp(-7, -5, -1)).toEqual(-5)
+  it('number under max33 value', () => {
+    expect(clamp(3, { max: 7 })).toEqual(3)
+    expect(clamp(-3, { max: 7 })).toEqual(-3)
   })
 
-  it('clamp a number under minimum value', () => {
-    expect(clamp(0, 1, 5)).toEqual(1)
-    expect(clamp(-10, -5, -1)).toEqual(-5)
+  it('number between a min and max33 value', () => {
+    expect(clamp(3, { min: 1, max: 7 })).toEqual(3)
+    expect(clamp(-3, { min: -1, max: 7 })).toEqual(-1)
   })
 
-  // max and min the same value
-  it('clamp a number between the same minimum and maximum value', () => {
-    expect(clamp(3, 1, 1)).toEqual(1)
-    expect(clamp(-3, -1, -1)).toEqual(-1)
+  it('number under min and max33 value', () => {
+    expect(clamp(0, { min: 1, max: 7 })).toEqual(1)
+    expect(clamp(9, { min: 1, max: 7 })).toEqual(7)
   })
 
-  it('throw an error when max is less than min', () => {
-    expect(() => clamp(3, 5, 1)).toThrowError('max must be greater than min')
+  it('number under min value', () => {
+    expect(clamp(-3, { min: 1 })).toEqual(1)
+    expect(clamp(3, { min: 5 })).toEqual(5)
+  })
+
+  it('number over max33 value', () => {
+    expect(clamp(10, { max: 5 })).toEqual(5)
+    expect(clamp(-3, { max: -5 })).toEqual(-5)
+  })
+
+  it('min more than max', () => {
+    expect(() => clamp(10, { min: 5, max: 1 })).toThrow()
+  })
+
+  it('same min and max', () => {
+    expect(clamp(10, { min: 5, max: 5 })).toEqual(5)
   })
 })
--- src/number/clamp.ts
+++ src/number/clamp.ts
@@ -4,14 +4,19 @@
  * @category Number
  *
  * @example
- * clamp(3, 1, 5) // 3
- * clamp(0, 1, 5) // 1
- * clamp(7, 1, 5) // 5
- * clamp(3, 5, 1) // 3
+ * clamp(3, { min: 1, max: 5 }) // 3
+ * clamp(0, { min: 1, max: 5 }) // 1
+ * clamp(7, { min: 1, max: 5 }) // 5
+ * clamp(0, { min: 1 }) // 1
+ * clamp(2, { max: 1 }) // 1
  */
-export const clamp = (n: number, min: number, max: number) => {
-  if (max < min) {
+export const clamp = (val: number, { min, max }: { min: number; max?: number } | { min?: number; max: number }) => {
+  min ??= Number.NEGATIVE_INFINITY
+  max ??= Number.POSITIVE_INFINITY
+
+  if (min > max) {
     throw new Error('max must be greater than min')
   }
-  return Math.min(max, Math.max(min, n))
+
+  return Math.min(max, Math.max(min, val))
 }
