7324ead2462b4ee188b1e954ebfa86c3c1f34511
Matija Osrečki <github@matijao.com>
2023-12-10 16:07:05 +0100

    feat: support tuple min max in clamp

--- src/number/clamp.test.ts
+++ src/number/clamp.test.ts
@@ -7,19 +7,25 @@ describe('clamp', () => {
     expect(clamp(-3, { min: -5 })).toEqual(-3)
   })
 
-  it('number under max33 value', () => {
+  it('number under max value', () => {
     expect(clamp(3, { max: 7 })).toEqual(3)
     expect(clamp(-3, { max: 7 })).toEqual(-3)
   })
 
-  it('number between a min and max33 value', () => {
+  it('number between a min and max value', () => {
     expect(clamp(3, { min: 1, max: 7 })).toEqual(3)
     expect(clamp(-3, { min: -1, max: 7 })).toEqual(-1)
+
+    expect(clamp(3, [1, 7])).toEqual(3)
+    expect(clamp(-3, [-1, 7])).toEqual(-1)
   })
 
-  it('number under min and max33 value', () => {
+  it('number under min and max value', () => {
     expect(clamp(0, { min: 1, max: 7 })).toEqual(1)
     expect(clamp(9, { min: 1, max: 7 })).toEqual(7)
+
+    expect(clamp(0, [1, 7])).toEqual(1)
+    expect(clamp(9, [-1, 7])).toEqual(7)
   })
 
   it('number under min value', () => {
@@ -27,16 +33,19 @@ describe('clamp', () => {
     expect(clamp(3, { min: 5 })).toEqual(5)
   })
 
-  it('number over max33 value', () => {
+  it('number over max value', () => {
     expect(clamp(10, { max: 5 })).toEqual(5)
     expect(clamp(-3, { max: -5 })).toEqual(-5)
   })
 
   it('min more than max', () => {
     expect(() => clamp(10, { min: 5, max: 1 })).toThrow()
+    expect(() => clamp(10, [5, 1])).toThrow()
   })
 
   it('same min and max', () => {
     expect(clamp(10, { min: 5, max: 5 })).toEqual(5)
+
+    expect(clamp(10, [5, 5])).toEqual(5)
   })
 })
--- src/number/clamp.ts
+++ src/number/clamp.ts
@@ -1,3 +1,5 @@
+import { isArray } from '..'
+
 /**
  * Clamp a number between a minimum and maximum value.
  *
@@ -9,10 +11,21 @@
  * clamp(7, { min: 1, max: 5 }) // 5
  * clamp(0, { min: 1 }) // 1
  * clamp(2, { max: 1 }) // 1
+ *
+ * clamp(3, [1, 5]) // 3
+ * clamp(0, [1, 5]) // 1
+ * clamp(7, [1, 5]) // 5
  */
-export const clamp = (val: number, { min, max }: { min: number; max?: number } | { min?: number; max: number }) => {
-  min ??= Number.NEGATIVE_INFINITY
-  max ??= Number.POSITIVE_INFINITY
+export function clamp(val: number, range: { min: number; max?: number } | { min?: number; max: number }): number
+export function clamp(val: number, range: [number, number]): number
+export function clamp(val: number, range: { min: number; max?: number } | { min?: number; max: number } | [number, number]): number {
+  let min: number, max: number
+
+  if (isArray(range)) {
+    [min = Number.NEGATIVE_INFINITY, max = Number.POSITIVE_INFINITY] = range
+  } else {
+    ({ min = Number.NEGATIVE_INFINITY, max = Number.POSITIVE_INFINITY } = range)
+  }
 
   if (min > max) {
     throw new Error('max must be greater than min')
