80bca75d7083bbdca2bc25f249bdf48ee231b956
Matija Osrečki <github@matijao.com>
2023-12-17 15:35:49 +0100

    feat: pass return type to deepOmit

--- src/object/deepOmit.ts
+++ src/object/deepOmit.ts
@@ -1,5 +1,5 @@
 import type { NestedOmit } from '..'
-import { clone, isEmpty, isObject } from '..'
+import { isEmpty, isObject } from '..'
 import { hasNestedKeys } from '.'
 
 /**
@@ -15,16 +15,19 @@ import { hasNestedKeys } from '.'
  * const obj = { a: 1, b: { c: 2, d: 3 } }
  * deepOmit(obj, { a: true, b: { c: true } })
  * // => { b: { d: 3 } }
+ *
+ * // preserve type
+ * type OmittedType = Omit<typeof obj, 'a' | 'b'> & { b: Omit<typeof obj.b, 'c'> };
+ * deepOmit<typeof obj, OmittedType>(obj, { a: true, b: { c: true } });
  */
-
-// TODO: return type not correct
 export const deepOmit = <
-  T extends Record<PropertyKey, any>
->(obj: T, keys: NestedOmit<T>) => {
-  if (!obj) { return {} as T }
+  T extends Record<PropertyKey, any>,
+  R extends Record<PropertyKey, any> = T
+>(obj: T, keys: NestedOmit<T>): R => {
+  if (!obj) { return {} as R }
   if (isEmpty(keys)) { return obj }
 
-  const result = hasNestedKeys(keys) ? clone(obj) : { ...obj }
+  const result = hasNestedKeys(keys) ? structuredClone(obj) : { ...obj }
 
   for (const key in keys) {
     const val = keys[key]
