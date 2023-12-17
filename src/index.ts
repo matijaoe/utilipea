e71a73b256310beff28fdd98a1f119b93164f5d1
Matija Osrečki <github@matijao.com>
2023-12-17 23:29:30 +0100

    chore: update deps, export all methods

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 export * from './array'
 export * from './async'
 export * from './collection'
+export * from './function'
 export * from './number'
 export * from './object'
 export * from './random'
+export * from './rest'
 export * from './string'
 export * from './typed'
 
