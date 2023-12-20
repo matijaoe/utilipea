3290da5c25c32aef3b44936e526523081920fef6
Matija Osrečki <github@matijao.com>
2023-12-21 00:56:30 +0100

    feat: simplify throttle

--- packages/package/src/async/throttle.ts
+++ packages/package/src/async/throttle.ts
@@ -9,73 +9,29 @@ export type ThrottledFunction<TArgs extends any[], TRes> = {
    */
   cancel(): void
   /**
-   * Runs the throttled function immediately
-   */
-  flush(): TRes | undefined
-  /**
    * Returns the last result of the throttled function
    */
   lastResult(): TRes | undefined
 }
 
-type ThrottleOptions = {
-  leading?: true
-  trailing?: boolean
-} | {
-  leading?: boolean
-  trailing: true
-}
-
-// i dont even figure out if this works correctly, for flush and trailing 🤷🏻
 export const throttle = <TArgs extends any[], TRes>(
   func: (...args: TArgs) => TRes,
   interval: number,
-  opts?: ThrottleOptions,
 ): ThrottledFunction<TArgs, TRes> => {
-  const { leading, trailing } = { leading: true, trailing: false, ...opts }
-
   let timer: ReturnType<typeof setTimeout> | undefined
-  let ready = leading
-  let lastArgs: TArgs | undefined
+  let ready = true
   let result: TRes | undefined
 
-  const invokeFunc = () => {
-    if (lastArgs) {
-      result = func(...lastArgs)
-      if (trailing) {
-        lastArgs = undefined
-      }
-    }
-    return result
-  }
-
-  const leadingEdge = () => {
-    if (ready && lastArgs) {
-      invokeFunc()
+  const throttled: ThrottledFunction<TArgs, TRes> = (...args: TArgs) => {
+    if (ready) {
+      result = func(...args)
       ready = false
     }
-    return result
-  }
-
-  const trailingEdge = () => {
-    timer = undefined
-
-    if (trailing && lastArgs) {
-      return invokeFunc()
-    }
-    lastArgs = undefined
-    return result
-  }
-
-  const throttled: ThrottledFunction<TArgs, TRes> = (...args: TArgs) => {
-    lastArgs = args
-
-    leadingEdge()
 
     if (!timer) {
       timer = setTimeout(() => {
-        trailingEdge()
-        ready = leading
+        timer = undefined
+        ready = true
       }, interval)
     }
 
@@ -96,11 +52,6 @@ export const throttle = <TArgs extends any[], TRes>(
     clearTimeout(timer)
     timer = undefined
     ready = false
-    lastArgs = undefined
-  }
-
-  throttled.flush = () => {
-    return !timer ? result : trailingEdge()
   }
 
   return throttled
