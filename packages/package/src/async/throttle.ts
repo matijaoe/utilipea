4206a27de8b139ece878a8ce29a6576fd4ee1432
Matija Osrečki <github@matijao.com>
2023-12-20 22:36:59 +0100

    feat: simplify throttle

--- packages/package/src/async/throttle.ts
+++ packages/package/src/async/throttle.ts
@@ -1,7 +1,7 @@
 // import type { ThrottledFunction } from '..'
 
 export type ThrottledFunction<TArgs extends any[], TRes> = {
-  (...args: TArgs): TRes | undefined
+  (...args: TArgs): void
   /**
    * Checks if there is any invocation throttled
    */
@@ -10,82 +10,48 @@ export type ThrottledFunction<TArgs extends any[], TRes> = {
    * Cancels the throttled function invocation
    */
   cancel(): void
-  /**
-   * Flushes the throttled function invocation
-   */
-  flush(): TRes | undefined
-  /**
-   * Returns the last result of the throttled function
-   */
-  lastResult: () => TRes | undefined
 }
 
 type ThrottleOptions = {
-  leading?: boolean
+  leading?: true
   trailing?: boolean
-  maxWait?: number
+} | {
+  leading?: boolean
+  trailing: true
 }
 
 export const throttle = <TArgs extends any[], TRes>(
   func: (...args: TArgs) => TRes,
   interval: number,
-  opts: ThrottleOptions = {},
+  opts?: ThrottleOptions,
 ): ThrottledFunction<TArgs, TRes> => {
-  opts = {
-    leading: true,
-    trailing: false,
-    ...opts
-  }
+  const { leading, trailing } = { leading: true, trailing: false, ...opts }
 
-  let maxWaitTimer: ReturnType<typeof setTimeout> | undefined
   let timer: ReturnType<typeof setTimeout> | undefined
-  let ready = opts.leading
-
-  let lastResult: TRes | undefined
+  let ready = leading
   let lastArgs: TArgs | undefined
 
   const throttled: ThrottledFunction<TArgs, TRes> = (...args: TArgs) => {
     if (ready) {
-      lastResult = func(...args)
+      func(...args)
       ready = false
-
-      if (maxWaitTimer) {
-        clearTimeout(maxWaitTimer)
-        maxWaitTimer = undefined
-      }
     }
 
-    if (opts.trailing) {
+    if (trailing) {
       lastArgs = args
     }
 
     if (!timer) {
+      clearTimeout(timer)
       timer = setTimeout(() => {
         if (lastArgs) {
-          lastResult = func(...lastArgs)
+          func(...lastArgs)
           lastArgs = undefined
         }
-        ready = opts.leading
+        ready = leading
         timer = undefined
       }, interval)
     }
-
-    if (opts.maxWait && !maxWaitTimer) {
-      maxWaitTimer = setTimeout(() => {
-        if (lastArgs) {
-          lastResult = func(...lastArgs)
-          lastArgs = undefined
-        }
-        ready = opts.leading
-        maxWaitTimer = undefined
-      }, opts.maxWait)
-    }
-
-    return lastResult
-  }
-
-  throttled.lastResult = () => {
-    return lastResult
   }
 
   throttled.pending = () => {
@@ -93,39 +59,12 @@ export const throttle = <TArgs extends any[], TRes>(
   }
 
   throttled.cancel = () => {
-    if (!timer && !maxWaitTimer) { return }
-    if (timer) {
-      clearTimeout(timer)
-      timer = undefined
-    }
-    if (maxWaitTimer) {
-      clearTimeout(maxWaitTimer)
-      maxWaitTimer = undefined
-    }
-    ready = opts.leading
+    if (!timer) { return }
+
+    clearTimeout(timer)
+    timer = undefined
+    ready = leading
     lastArgs = undefined
-  }
-
-  throttled.flush = () => {
-    if (!timer && !maxWaitTimer) { return }
-
-    if (timer) {
-      clearTimeout(timer)
-      timer = undefined
-    }
-
-    if (maxWaitTimer) {
-      clearTimeout(maxWaitTimer)
-      maxWaitTimer = undefined
-    }
-
-    if (lastArgs) {
-      lastResult = func(...lastArgs)
-      lastArgs = undefined
-    }
-
-    ready = opts.leading
-    return lastResult
   }
 
   return throttled
