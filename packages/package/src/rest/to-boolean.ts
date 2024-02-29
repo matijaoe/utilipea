f2768f857f26fe8928d2442e608f0e2210cc6209
Matija Osrečki <github@matijao.com>
2024-02-29 21:19:25 +0100

    feat: update deps, improve to-boolean

--- packages/package/src/rest/to-boolean.ts
+++ packages/package/src/rest/to-boolean.ts
@@ -3,21 +3,22 @@ import type { EnumType } from './enum'
 
 export const TrueSet = {
   basic: ['true'],
+  numeric: ['1'],
   full: ['true', 't', 'yes', 'y', 'on', '1'],
-  number: [1]
 } as const
 
 export const FalseSet = {
   basic: ['false'],
+  numeric: ['0'],
   full: ['false', 'f', 'no', 'n', 'off', '0'],
-  number: [0]
-} as const
+} as const 
 
 export type TrueValues = EnumType<typeof TrueSet>
 export type FalseValues = EnumType<typeof FalseSet>
 
-export type ToBooleanOptions = {
+export type ToBooleanOptions<TDefaultValue = boolean> = {
   strict?: boolean
+  defaultValue?: TDefaultValue
   trueValues?: ReadonlyArray<TrueValues | string>
   falseValues?: ReadonlyArray<FalseValues | string>
 }
@@ -27,30 +28,44 @@ export type ToBooleanOptions = {
 // toBooleanExact ?
 export const parseBooleanQuery = (val: string) => !['false', '0', ''].includes(val)
 
-export const toBoolean = (val: any, options?: ToBooleanOptions): val is boolean => {
+export const toBoolean = <TDefaultValue>(val: any, options?: ToBooleanOptions<TDefaultValue>): TDefaultValue | boolean => {
+  options ??= {}
   const {
+    strict = false,
+  } = options
+  let {
+    // will only be used when value is string
     trueValues = TrueSet.basic,
     falseValues = FalseSet.basic,
-    strict = false,
-  } = options ?? {}
+  } = options
 
-  if (isBoolean(val)) { return val }
+  // TODO: could me improved
+  const defaultValue = 'defaultValue' in options 
+    ? options.defaultValue as TDefaultValue
+    : false
+
+  trueValues = trueValues.map((v) => String(v).trim().toLowerCase())
+  falseValues = trueValues.map((v) => String(v).trim().toLowerCase())
+
+  if (isBoolean(val)) { 
+    return val 
+  }
 
   if (isNumber(val)) {
     if (strict && val !== 1 && val !== 0) {
-      throw new Error(`Cannot convert ${val} to boolean`)
+      throw new Error(`Cannot convert number ${val} to boolean`)
     }
     return val === 1
   }
 
   if (isString(val)) {
-    val = val.trim().toLowerCase()
-    if (trueValues.includes(val as string)) { return true }
-    if (falseValues.includes(val as string)) { return false }
+    const normalizedVal = val.trim().toLowerCase()
+    if (trueValues.includes(normalizedVal)) { return true }
+    if (falseValues.includes(normalizedVal)) { return false }
     if (strict) {
-      throw new Error(`Cannot convert "${val}" to boolean`)
+      throw new Error(`Cannot convert string "${normalizedVal}" to boolean`)
     }
   }
 
-  return false
+  return defaultValue
 }
